2dc5bb37027204abe6f9472647b64d1425519526
Abhishekjain0112 <abhishekjain0112@gmail.com>
2020-08-04 03:57:25 +0530

    Random quick sort implementation  start

---
+++ Java Algorithums/src/coursera/Sorting.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursera;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Abhishek
+ */
+public class Sorting {
+
+    
+    private static Random random = new Random();
+
+    private static int[] partition3(int[] a, int l, int r) {
+      //write your code here
+
+
+      int m1 = l;
+      int m2 = r;
+        int x = a[l];
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (a[i] <= x) {
+                j++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[l];
+        a[l] = a[j];
+        a[j] = t;      
+      
+      for(int i=l;i<=r;i++){
+         if(a[i]==x)
+         {
+             m1=i;
+             break;
+         }
+      }
+      
+      
+      int[] m = {m1, j};
+  System.out.println("Quick Sort "+ Arrays.toString(a)+" start ind :"+l +" end : "+r+"  pivot :"+a[l] +" return : "+Arrays.toString(m));    
+    
+      return m;
+    }
+
+    private static int partition2(int[] a, int l, int r) {
+        int x = a[l];
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (a[i] <= x) {
+                j++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[l];
+        a[l] = a[j];
+        a[j] = t;
+        return j;
+    }
+
+    private static void randomizedQuickSort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+      //  int k = random.nextInt(r - l + 1) + l;
+       // int t = a[l];
+       // a[l] = a[k];
+        //a[k] = t;
+        //use partition3
+        int []m = partition3(a, l, r);
+        randomizedQuickSort(a, l, m[0] - 1);
+        randomizedQuickSort(a, m[1] + 1, r);
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        randomizedQuickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+    
+    
