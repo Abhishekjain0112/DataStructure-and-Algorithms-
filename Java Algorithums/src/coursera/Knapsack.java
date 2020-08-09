b3c47cbcc1239a8919c9a85e9e2b6c3385c932d9
Abhishekjain0112 <abhishekjain0112@gmail.com>
2020-08-10 03:48:46 +0530

    Gold Knapsack Problem solution

--- Java Algorithums/src/coursera/Knapsack.java
+++ Java Algorithums/src/coursera/Knapsack.java
@@ -5,6 +5,8 @@
  */
 package coursera;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Abhishek
@@ -12,13 +14,49 @@ package coursera;
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
+        
         int result = 0;
+       int items=w.length;
+       int value[][]=new int [items+1][W+1];
+        
+       for(int i=0;i<items+1;i++)
+           value[i][0]=0;
+       
+       for(int j=0;j<W+1;j++)
+           value[0][j]=0;
+        
+       
+       for(int i =1;i<=items;i++){
+         
+         for(int weight=1;weight<W+1;weight++){
+             
+           value[i][weight]= value[i-1][weight];  
+           
+             if(w[i-1]<=weight){
+               int maxvalue=value[i-1][weight-w[i-1]]+w[i-1];
+               
+                if(value[i][weight]<maxvalue)
+                    value[i][weight]=maxvalue;
+                 }
+             }
+        }
+       
+                /*
+             for(int i =0; i<=items;i++){
+               for(int j=0;j<=W;j++){
+                   System.out.print(" "+value[i][j]);
+               }System.out.println("");
+             }   */    
+       
+       
+       
+        /*
         for (int i = 0; i < w.length; i++) {
           if (result + w[i] <= W) {
             result += w[i];
           }
-        }
-        return result;
+        }*/
+        return value[items][W];
     }
 
     public static void main(String[] args) {
