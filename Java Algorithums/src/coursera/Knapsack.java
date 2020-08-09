1bfcb5937fe8aaf04022d0f4b2c04632ce8befdd
Abhishekjain0112 <abhishekjain0112@gmail.com>
2020-08-09 23:21:19 +0530

    knapsack

---
+++ Java Algorithums/src/coursera/Knapsack.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursera;
+
+/**
+ *
+ * @author Abhishek
+ */
+public class Knapsack {
+    static int optimalWeight(int W, int[] w) {
+        //write you code here
+        int result = 0;
+        for (int i = 0; i < w.length; i++) {
+          if (result + w[i] <= W) {
+            result += w[i];
+          }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int W, n;
+        W = scanner.nextInt();
+        n = scanner.nextInt();
+        int[] w = new int[n];
+        for (int i = 0; i < n; i++) {
+            w[i] = scanner.nextInt();
+        }
+        System.out.println(optimalWeight(W, w));
+    }
+}
+
