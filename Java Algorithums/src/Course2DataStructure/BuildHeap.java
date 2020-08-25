9f736fc5e991f424e261a7c590fbda0a7824f01e
Abhishekjain0112 <abhishekjain0112@gmail.com>
2020-08-25 18:42:32 +0530

    BuildHeap problem

---
+++ Java Algorithums/src/Course2DataStructure/BuildHeap.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Course2DataStructure;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Abhishek
+ */
+
+public class BuildHeap {
+    private int[] data;
+    private List<Swap> swaps;
+
+    private FastScanner in;
+    private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        new BuildHeap().solve();
+    }
+
+    private void readData() throws IOException {
+        int n = in.nextInt();
+        data = new int[n];
+        for (int i = 0; i < n; ++i) {
+          data[i] = in.nextInt();
+        }
+    }
+
+    private void writeResponse() {
+        out.println(swaps.size());
+        for (Swap swap : swaps) {
+          out.println(swap.index1 + " " + swap.index2);
+        }
+    }
+
+    private void generateSwaps() {
+      swaps = new ArrayList<Swap>();
+      
+      // The following naive implementation just sorts 
+      // the given sequence using selection sort algorithm
+      // and saves the resulting sequence of swaps.
+      // This turns the given array into a heap, 
+      // but in the worst case gives a quadratic number of swaps.
+      // TODO: replace by a more efficient implementation
+	  
+      
+      /*
+      for (int i = 0; i < data.length; ++i) {
+        for (int j = i + 1; j < data.length; ++j) {
+          if (data[i] > data[j]) {
+            swaps.add(new Swap(i, j));
+            int tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
+          }
+        }
+      }*/
+      int n =data.length;
+    for(int i=n/2;i>=0; i--){
+        int lowest=i;
+        int left=i*2+1;
+        int right=i*2+2;
+        
+        if(left<n  && data[left]<data[lowest])
+            lowest=left;
+        if(right<n && data[right]<data[lowest])
+            lowest=right;
+        
+        if(lowest!=i){
+        swaps.add(new Swap(i, lowest));
+        int swap=data[lowest];
+        data[lowest]=data[i];
+        data[i]=swap;
+        }
+        
+    }  
+      
+      
+      
+    }
+
+    
+    
+    
+    
+    public void solve() throws IOException {
+        in = new FastScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        readData();
+        generateSwaps();
+        writeResponse();
+        out.close();
+    }
+
+    static class Swap {
+        int index1;
+        int index2;
+
+        public Swap(int index1, int index2) {
+            this.index1 = index1;
+            this.index2 = index2;
+        }
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
