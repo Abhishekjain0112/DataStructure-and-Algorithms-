fd00b769a12aa02479dcad0f4a93c5074611224f
Abhishekjain0112 <abhishekjain0112@gmail.com>
2020-09-10 23:34:07 +0530

    AVL TREE

--- Java Algorithums/src/DataStructure/BinaryTree.java
+++ Java Algorithums/src/DataStructure/BinaryTree.java
@@ -31,10 +31,53 @@ public class BinaryTree {
         BinaryTree tree =new BinaryTree();
         for(int i=1;i<=n;i++){
             System.out.print("Add Element "+i+" :");
-         tree.insert(sc.nextInt());
+          // tree.insert(sc.nextInt());
+            tree.insertbyRecurtion(tree.root,sc.nextInt());
         }
+        
+        System.out.println("InOrder");
+        tree.inOrder(tree.root);
+        System.out.println("\n PreOrder");
+        tree.PreOrder(tree.root);
+        
+        System.out.println("\n PostOrder");
+        tree.postOrder(tree.root);
+        
     }
-        //insert
+    
+    //insert by recurtion
+    void insertbyRecurtion(Node root,int data){
+      
+        if(this.root==null)
+        {
+         this.root=new Node(data);
+         return;
+        }
+       if(data<root.key){
+        
+           if(root.left==null)
+           {
+              root.left=new Node(data);
+              return;
+           }else
+           insertbyRecurtion(root.left,data);
+       
+       }else{
+       
+         if(root.right==null){
+           root.right=new Node(data);
+           return;
+         }else
+             insertbyRecurtion(root.right, data);
+
+       }
+     
+    } 
+    
+    
+    
+    
+        //insert by loop 
     public void insert(int info){
         
        Node node=new Node(info);
@@ -79,6 +122,21 @@ public class BinaryTree {
     }    
  
  
- 
+ void PreOrder(Node node){
+  if(node==null)
+      return;
+     System.out.print(" "+node.key);
+     PreOrder(node.left);
+     PreOrder(node.right);      
+ }
     
+ void postOrder(Node node){
+     if(node==null)
+         return;
+     postOrder(node.left);
+     postOrder(node.right);
+     System.out.print(" " +node.key);
+ }
+ 
+ 
 }
